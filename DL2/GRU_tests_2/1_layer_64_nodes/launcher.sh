2769e0397d7e395e42f21501135fad6f44c5a353
Diego S <cuquiwi@hotmail.com>
2019-11-13 13:13:02 +0100

    results GRU

--- DL2/GRU_tests_2/1_layer_64_nodes/launcher.sh
+++ DL2/GRU_tests_2/1_layer_64_nodes/launcher.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-#SBATCH --job-name="DL2_SENNE"
+#SBATCH --job-name="DL2_Diego"
 
 #SBATCH -D .
 
 #SBATCH --workdir=.
 
-#SBATCH --output=DL1_%j.out
+#SBATCH --output=DL2_%j.out
 
-#SBATCH --error=DL1_%j.err
+#SBATCH --error=DL2_%j.err
 
 #SBATCH --ntasks=40
 
 #SBATCH --gres gpu:1
 
-#SBATCH --time=20:00:00
+#SBATCH --time=00:50:00
 
 module purge; module load K80/default impi/2018.1 mkl/2018.1 cuda/8.0 CUDNN/7.0.3 python/3.6.3_ML
 
